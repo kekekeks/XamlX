1eec346f76a9e74d88f82b08810c3bf943b0d0cd
Nikita Tsukanov <keks9n@gmail.com>
2019-02-10 00:15:41 +0300

    Added benchmarks from Portable.Xaml

---
+++ src/Benchmarks/LoadBenchmark.cs
@@ -0,0 +1,59 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Benchmarks
+{
+	public interface IXamlBenchmark
+	{
+		
+	}
+	public abstract class LoadBenchmark : IXamlBenchmark
+	{
+		public abstract string TestName { get; }
+
+		protected Stream GetStream() => typeof(IXamlBenchmark).Assembly.GetManifestResourceStream("Benchmarks." + TestName);
+
+		Portable.Xaml.XamlSchemaContext pxc;
+		[Benchmark(Baseline = true)]
+		public void PortableXaml()
+		{
+			pxc = pxc ?? new Portable.Xaml.XamlSchemaContext();
+			using (var stream = GetStream())
+				Portable.Xaml.XamlServices.Load(new Portable.Xaml.XamlXmlReader(stream, pxc));
+		}
+/*
+		System.Xaml.XamlSchemaContext sxc;
+		[Benchmark]
+		public void SystemXaml()
+		{
+			sxc = sxc ?? new System.Xaml.XamlSchemaContext();
+			using (var stream = GetStream())
+				System.Xaml.XamlServices.Load(new System.Xaml.XamlXmlReader(stream, sxc));
+		}
+*/
+		[Benchmark]
+		public void PortableXamlNoCache()
+		{
+			using (var stream = GetStream())
+				Portable.Xaml.XamlServices.Load(stream);
+		}
+/*
+		[Benchmark]
+		public void SystemXamlNoCache()
+		{
+			using (var stream = GetStream())
+				System.Xaml.XamlServices.Load(stream);
+		}
+*/
+		private Func<IServiceProvider, object> _compiled;
+		[Benchmark]
+		public void XamlX()
+		{
+			_compiled = _compiled ?? BenchCompiler.Compile(new StreamReader(GetStream()).ReadToEnd());
+			_compiled(null);
+		}
+		
+	}
+}
---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			/**  Uncomment to test using performance profiler */
+			if (args?.FirstOrDefault() == "profile")
+			{
+				//var benchmark = new LoadSimpleBenchmark();
+				var benchmark = new LoadComplexBenchmark();
+				benchmark.XamlX();
+				for (int i = 0; i < 1000; i++)
+				{
+					benchmark.XamlX();
+				}
+				return;
+			}
+			/**/
+
+			// BenchmarkSwitcher doesn't automatically exclude abstract benchmark classes
+			var types = typeof(MainClass)
+				.Assembly
+				.GetExportedTypes()
+				.Where(r => typeof(IXamlBenchmark).IsAssignableFrom(r) && !r.IsAbstract);
+
+
+			var config = ManualConfig
+				.Create(DefaultConfig.Instance)
+				.With(Job.Default)
+				.With(MemoryDiagnoser.Default)
+				.With(StatisticColumn.OperationsPerSecond)
+				.With(RankColumn.Arabic);
+
+			var switcher = new BenchmarkSwitcher(types.ToArray());
+			switcher.Run(args, config);
+		}
+	}
+}
