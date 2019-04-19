c1ffc6cd09496a48b58bcf0ff1f0163bc86ce714
Nikita Tsukanov <keks9n@gmail.com>
2019-04-19 11:09:03 +0300

    Expose line info from XamlXParseException

--- src/XamlX/Exceptions.cs
+++ src/XamlX/Exceptions.cs
@@ -5,9 +5,13 @@ namespace XamlX
 {
     public class XamlParseException : Exception
     {
+        public int Line { get; }
+        public int Position { get; }
+        
         public XamlParseException(string message, int line, int position) : base($"{message} (line {line} position {position})")
         {
-            
+            Line = line;
+            Position = position;
         }
 
         public XamlParseException(string message, IXamlLineInfo lineInfo) : this(message, lineInfo.Line, lineInfo.Position)
