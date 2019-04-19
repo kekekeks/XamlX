1b26cc5649a5ead67bd77ed7dfa1784ae71d06b4
Nikita Tsukanov <keks9n@gmail.com>
2019-04-19 11:29:58 +0300

    Fixed x:Null to Nullable<T> conversion

--- src/XamlX/TypeSystem/TypeSystemHelpers.cs
+++ src/XamlX/TypeSystem/TypeSystemHelpers.cs
@@ -103,20 +103,20 @@ namespace XamlX.TypeSystem
             else if (what == XamlPseudoType.Null)
             {
                 
-                if (what.IsValueType)
+                if (to.IsValueType)
                 {
-                    if (what.GenericTypeDefinition.Equals(context.Configuration.WellKnownTypes.NullableT))
+                    if (to.GenericTypeDefinition.Equals(context.Configuration.WellKnownTypes.NullableT))
                     {
-                        using (var loc = context.GetLocal(what))
+                        using (var loc = context.GetLocal(to))
                             ld(false)
                                 .Pop()
                                 .Ldloca(loc.Local)
-                                .Emit(OpCodes.Initobj, what)
+                                .Emit(OpCodes.Initobj, to)
                                 .Ldloc(loc.Local);
 
                     }
                     else
-                        throw new XamlLoadException("Unable to convert {x:Null} to " + what.GetFqn(), node);
+                        throw new XamlLoadException("Unable to convert {x:Null} to " + to.GetFqn(), node);
                 }
                 else
                     ld(false);
