e8417bed42d72b0d79f324fc65e0d291697a485a
Nikita Tsukanov <keks9n@gmail.com>
2019-04-21 13:02:18 +0300

    Add Blend2008 namespace

--- src/XamlIl/XamlNamespaces.cs
+++ src/XamlIl/XamlNamespaces.cs
@@ -3,5 +3,6 @@ namespace XamlIl
     public class XamlNamespaces
     {
         public const string Xaml2006 = "http://schemas.microsoft.com/winfx/2006/xaml";
+        public const string Blend2008 = "http://schemas.microsoft.com/expression/blend/2008";
     }
 }
