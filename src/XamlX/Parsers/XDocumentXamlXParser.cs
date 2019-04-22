b815aaa1ac874642df092e06732e303ee9bc8626
Nikita Tsukanov <keks9n@gmail.com>
2019-04-22 15:31:06 +0300

    Trim whitespace

--- src/XamlX/Parsers/XDocumentXamlXParser.cs
+++ src/XamlX/Parsers/XDocumentXamlXParser.cs
@@ -208,7 +208,7 @@ namespace XamlX.Parsers
                 if (node is XElement el)
                     return ParseNewInstance(el, false);
                 if (node is XText text)
-                    return new XamlXAstTextNode(node.AsLi(), text.Value);
+                    return new XamlXAstTextNode(node.AsLi(), text.Value.Trim());
                 return null;
             }
 
