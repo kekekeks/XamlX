3ff6c35e51e39b45cd73becb40eee7619fc29cca
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2019-07-06 20:52:27 -0700

    Fix some bugs I found while testing.

--- src/XamlIl.Cecil/CecilMethod.cs
+++ src/XamlIl.Cecil/CecilMethod.cs
@@ -13,53 +13,54 @@ namespace XamlIl.TypeSystem
             public MethodReference Reference { get; }
             public MethodReference IlReference { get; }
             public MethodDefinition Definition { get; }
-            private TypeReference _declaringTypeReference;
-            
+            protected readonly TypeReference _declaringTypeReference;
 
-            public CecilMethodBase(CecilTypeSystem typeSystem, MethodDefinition method, TypeReference declaringType)
+            public CecilMethodBase(CecilTypeSystem typeSystem, MethodReference method, TypeReference declaringType)
             {
                 TypeSystem = typeSystem;
 
                 MethodReference MakeRef(bool transform)
                 {
                     TypeReference Transform(TypeReference r) => transform ? r.TransformGeneric(declaringType) : r;
-                    
+
                     var reference = new MethodReference(method.Name, Transform(method.ReturnType),
                         declaringType)
                     {
                         HasThis = method.HasThis,
                         ExplicitThis = method.ExplicitThis,
                     };
+
                     foreach (ParameterDefinition parameter in method.Parameters)
                         reference.Parameters.Add(
                             new ParameterDefinition(Transform(parameter.ParameterType)));
 
                     foreach (var genericParam in method.GenericParameters)
                         reference.GenericParameters.Add(new GenericParameter(genericParam.Name, reference));
+
+                    if (method is GenericInstanceMethod generic)
+                    {
+                        var genericReference = new GenericInstanceMethod(reference);
+                        foreach (var genericArg in generic.GenericArguments)
+                        {
+                            genericReference.GenericArguments.Add(Transform(genericArg));
+                        }
+                        reference = genericReference;
+                    }
+
                     return reference;
                 }
 
                 Reference = MakeRef(true);
                 IlReference = MakeRef(false);
-                Definition = method;
+                Definition = method.Resolve();
                 _declaringTypeReference = declaringType;
-            }
-
-            public CecilMethodBase(CecilTypeSystem typeSystem, GenericInstanceMethod method)
-            {
-                TypeSystem = typeSystem;
-                Reference = method;
-                IlReference = method.ElementMethod;
-                Definition = method.ElementMethod.Resolve();
-                _declaringTypeReference = Definition.DeclaringType;
             }
             
             public string Name => Reference.Name;
             public bool IsPublic => Definition.IsPublic;
             public bool IsStatic => Definition.IsStatic;
-            private IXamlIlType _returnType;
 
-            
+            private IXamlIlType _returnType;
             
             public IXamlIlType ReturnType =>
                 _returnType ?? (_returnType = TypeSystem.Resolve(Reference.ReturnType));
@@ -84,13 +85,8 @@ namespace XamlIl.TypeSystem
         [DebuggerDisplay("{" + nameof(Reference) + "}")]
         class CecilMethod : CecilMethodBase, IXamlIlMethodBuilder
         {
-            public CecilMethod(CecilTypeSystem typeSystem, MethodDefinition methodDef,
-                TypeReference declaringType) : base(typeSystem, methodDef, declaringType)
-            {
-            }
-
-            private CecilMethod(CecilTypeSystem typeSystem, GenericInstanceMethod method)
-                : base(typeSystem, method)
+            public CecilMethod(CecilTypeSystem typeSystem, MethodReference methodRef,
+                TypeReference declaringType) : base(typeSystem, methodRef, declaringType)
             {
             }
 
@@ -103,15 +99,13 @@ namespace XamlIl.TypeSystem
             public IXamlIlMethod MakeGenericMethod(IReadOnlyList<IXamlIlType> typeArguments)
             {
                 GenericInstanceMethod instantiation = new GenericInstanceMethod(Reference);
-                if (Reference == Definition)
+                foreach (var type in typeArguments.Cast<ITypeReference>().Select(r => r.Reference))
                 {
-                    foreach (var type in typeArguments.Cast<ITypeReference>().Select(r => r.Reference))
-                    {
-                        instantiation.GenericArguments.Add(type);
-                    }
+                    instantiation.GenericParameters.Add(new GenericParameter(Reference));
+                    instantiation.GenericArguments.Add(type);
                 }
 
-                return new CecilMethod(TypeSystem, instantiation);
+                return new CecilMethod(TypeSystem, instantiation, _declaringTypeReference);
             }
         }
         
@@ -125,28 +119,6 @@ namespace XamlIl.TypeSystem
 
             public bool Equals(IXamlIlConstructor other) => other is CecilConstructor cm
                                                             && cm.Reference.Equals(Reference);
-        }
-
-        class UnresolvedMethod : IXamlIlMethod
-        {
-            public UnresolvedMethod(string name)
-            {
-                Name = name;
-            }
-            
-            public bool Equals(IXamlIlMethod other) => other == this;
-
-            public IXamlIlMethod MakeGenericMethod(IReadOnlyList<IXamlIlType> typeArguments)
-            {
-                return new UnresolvedMethod(Name);
-            }
-
-            public string Name { get; }
-            public bool IsPublic { get; }
-            public bool IsStatic { get; }
-            public IXamlIlType ReturnType { get; } = XamlIlPseudoType.Unknown;
-            public IReadOnlyList<IXamlIlType> Parameters { get; } = new IXamlIlType[0];
-            public IXamlIlType DeclaringType { get; } = XamlIlPseudoType.Unknown;
         }
     }
 }
