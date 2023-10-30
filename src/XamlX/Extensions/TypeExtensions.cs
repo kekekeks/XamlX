a5302eff48f1b4409e0563065d3ae97502feab0c
workgroupengineering <workgroupengineering@users.noreply.github.com>
2023-10-30 15:10:11 +0100

    fix: AvaloniaUI/Avalonia#12974 - Preview not showed when using using statment (#94)
    * fix: AvaloniaUI/Avalonia#12974 - Preview not showed when using using statment
    
    * fix: Address review

---
+++ src/XamlX/Extensions/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace System;
+
+/// <summary>
+/// Source https://stackoverflow.com/a/46382137/20894223
+/// </summary>
+internal static class TypeExtensions
+{
+    public static bool IsTopLevelInternal(this Type t) => !t.IsVisible
+            && !t.IsPublic
+            && t.IsNotPublic
+            && !t.IsNested
+            && !t.IsNestedPublic
+            && !t.IsNestedFamily
+            && !t.IsNestedPrivate
+            && !t.IsNestedAssembly
+            && !t.IsNestedFamORAssem
+            && !t.IsNestedFamANDAssem;
+}
