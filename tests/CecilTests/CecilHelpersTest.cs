ace18a873b4a950cc613067487dc2a0854cc1593
Max Katz <maxkatz6@outlook.com>
2024-04-15 17:13:41 -0700

    Add new tests for cecil helpers

--- tests/CecilTests/CecilHelpersTest.cs
+++ tests/CecilTests/CecilHelpersTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Mono.Cecil;
 using XamlParserTests;
@@ -51,6 +52,40 @@ namespace CecilTests
             var type2 = Configuration.TypeSystem.GetType("System.String");
             Assert.True(type1.Equals(type2));
             Assert.True(type2.Equals(type1));
+        }
+
+        [Fact]
+        public void Dictionary_With_Generics_Should_Not_Equal_To_Plain()
+        {
+            var type1 = MakeGenericType("System.Collections.Generic.Dictionary`2", "System.String", "System.String");
+            var type2 = Configuration.TypeSystem.GetType("System.Collections.Generic.Dictionary`2");
+            Assert.False(type1.Equals(type2));
+            Assert.False(type2.Equals(type1));
+        }
+
+        [Fact]
+        public void Dictionary_Base_Interface_Should_Be_Completely_Resolved()
+        {
+            var type1 = MakeGenericType("System.Collections.Generic.Dictionary`2", "System.String", "System.String")
+                .GetAllInterfaces().First(i => i.Name.StartsWith("ICollection`"));
+            var type2 = Configuration.TypeSystem.GetType("System.Collections.Generic.ICollection`1")
+                .MakeGenericType(Configuration.TypeSystem.GetType("System.Collections.Generic.KeyValuePair`2")
+                    .MakeGenericType(Configuration.TypeSystem.GetType("System.String"), Configuration.TypeSystem.GetType("System.String")));
+            Assert.True(type1.Equals(type2));
+            Assert.True(type2.Equals(type1));
+        }
+
+        [Fact]
+        public void Dictionary_ICollection_Methods_Should_Be_Completely_Resolved()
+        {
+            var type = MakeGenericType("System.Collections.Generic.Dictionary`2", "System.String", "System.String");
+
+            var collectionMethod = type.FindMethod(m => m.Name == "Add" && m.Parameters.Count == 1);
+            var inputParam = collectionMethod.Parameters[0];
+            var expectedParamType = Configuration.TypeSystem.GetType("System.Collections.Generic.KeyValuePair`2")
+                .MakeGenericType(Configuration.TypeSystem.GetType("System.String"), Configuration.TypeSystem.GetType("System.String"));
+            Assert.True(expectedParamType.Equals(inputParam));
+            Assert.True(inputParam.Equals(expectedParamType));
         }
 
         private IXamlType MakeGenericType(string baseType, params string[] genericArgs)
