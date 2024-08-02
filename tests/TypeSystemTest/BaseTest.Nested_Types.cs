e105c0695441664e24e4eeb7dd58996823d215e8
Giuseppe Lippolis <workgroupengineering@outlook.com>
2024-08-02 10:33:49 +0200

    test: add test case

--- tests/TypeSystemTest/BaseTest.Nested_Types.cs
+++ tests/TypeSystemTest/BaseTest.Nested_Types.cs
@@ -8,6 +8,7 @@ partial class BaseTest
     protected const string NestedTypesNamespace = $"{ModelsNamespace}.{nameof(Models.NestedTyeps)}";
     protected const string PublicNestedTypeName = $"{NestedTypesNamespace}.{nameof(Models.NestedTyeps.PublicNestedTypeContainer)}";
     protected const string InternalNestedTypeName = $"{NestedTypesNamespace}.{nameof(Models.NestedTyeps.InternaNestedTypeContainer)}";
+    protected const string PrivateNestedTypeName = $"{NestedTypesNamespace}.{nameof(Models.NestedTyeps.PrivateNestedTypeContainer)}";
 
     [Theory]
     [InlineData($"{PublicNestedTypeName}+{nameof(Models.NestedTyeps.PublicNestedTypeContainer.PublicNestedType)}", false)]
@@ -16,6 +17,7 @@ partial class BaseTest
     [InlineData($"{InternalNestedTypeName}+{nameof(Models.NestedTyeps.PublicNestedTypeContainer.PublicNestedType)}", false)]
     [InlineData($"{InternalNestedTypeName}+{nameof(Models.NestedTyeps.PublicNestedTypeContainer.InternalNestedType)}", false)]
     [InlineData($"{InternalNestedTypeName}+PrivateNestedType", true)]
+    [InlineData($"{PrivateNestedTypeName}+NestedPrivate+NestedPublic", true)]
     public void Should_Handled_Nested_Type(string typeName, bool throwXamlTypeSystemException)
     {
         System.Exception? exception = null;
